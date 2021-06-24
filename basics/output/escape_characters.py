1d2dc6e30c03446f8cf67c5703cdd6b677b6cea8
OctavianSta <>
2021-06-24 12:02:54 +0000

    Added code to display escape characters.

---
+++ basics/output/escape_characters.py
@@ -0,0 +1,6 @@
+#Display escape the characters
+print("\n Displays a new line") 
+print("\t Displays a tab space") 
+print("\\ Displays a back slash") 
+print("\" Displays a double quote") 
+print("\' Displays a single quote")
