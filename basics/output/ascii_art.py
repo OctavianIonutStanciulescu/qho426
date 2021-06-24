1da8807cbd94c12b505100fc26a9ca32a2ab3c49
OctavianSta <>
2021-06-24 12:44:50 +0000

    Added ascii_art.py as a representation of Beep

---
+++ basics/output/ascii_art.py
@@ -0,0 +1,9 @@
+#Display an image of Beep
+print ("\t ###")
+print ("\t # #  I will be watching your progress!!!")
+print ("\t ###")
+print ("   ///|///")
+print ("\t |||")
+print ("\t |||")
+print ("\t |||")
+print ("    || ||")
