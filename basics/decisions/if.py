9ccb065ab72bb9957026ec225900949c9513cd27
OctavianSta <>
2021-07-01 21:25:16 +0000

    Added code to demonstrate the use of an if statement

---
+++ basics/decisions/if.py
@@ -0,0 +1,6 @@
+#Using the if function for the first time
+adventure = True
+print("What type of book is this?")
+book = str(input())
+if adventure:
+  print("\nI like adventure books!\n\nFinished reading book.")
--- main.py
+++ main.py
@@ -1,2 +1,6 @@
-print('Bazzinga3!')
-print("Octavian")
+# #Using the if function for the first time
+# adventure = True
+# print("What type of book is this?")
+# book = str(input())
+# if adventure:
+#   print("\nI like adventure books!\n\nFinished reading book.")
