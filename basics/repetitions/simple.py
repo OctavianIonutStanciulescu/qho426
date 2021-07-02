8d405a59e44d01947e994c115a5e16bde1fbb060
OctavianSta <>
2021-07-02 12:45:17 +0000

    Added code to demonstrate the use of a simple while loop.

---
+++ basics/repetitions/simple.py
@@ -0,0 +1,7 @@
+#using the while repetitions
+print("How many cables should I remove?") 
+r1 = int(input())
+counter = 1
+while counter <= r1:
+  print ("Removed cable.")
+  counter +=1
--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-#using the logical AND operator
-print("What did i hear?") 
-r1 = input() 
-print("\nWhat did i see?")
-r2 = input()
-if ((r1 == "grrr") and (r2 == "two red eyes")): 
-  print("\nThere is a scary creature. I should get out of here!")
-else:
-  print("\nI am a little scared but i will continue.")
+# #using the while repetitions
+# print("How many cables should I remove?") 
+# r1 = int(input())
+# counter = 1
+# while counter <= r1:
+#   print ("Removed cable.")
+#   counter +=1
