22b68b1f702995475b8289bf4fde0a127f6a14ca
OctavianSta <>
2021-07-02 09:32:19 +0000

    Added code to demonstrate hoe a variable can be used as a counter.

---
+++ basics/decisions/simple_decision/comparison_operators.py
@@ -0,0 +1,7 @@
+# Comparison operator function
+first = int(input("Please enter the first number:\n"))
+second = int(input("Please enter the second number:\n"))
+if first > second:
+  print ("\nThe second number is the smallest.")
+else:
+  print ("\nThe first number is the smallest.")
