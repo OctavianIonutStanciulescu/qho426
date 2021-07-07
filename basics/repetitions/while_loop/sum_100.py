5dc931f60e65fe03c96057dec0285766f1a41ad7
OctavianSta <>
2021-07-07 07:46:00 +0000

    Added code to demonstrate the use of summing with a while loop

---
+++ basics/repetitions/while_loop/sum_100.py
@@ -0,0 +1,8 @@
+#using sum while in a loop
+print("Calculating the sum of the first 100 numbers...") 
+counter = 0
+y = 0
+while counter <= 100:
+ y += counter
+ counter +=1
+print("...Done! The answer is {}." .format(y))
--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-# #using the len function
-# print("Please enter a phrase:") 
-# p =input()
-# print("Bop "*len(p))
+#using sum while in a loop
+print("How many numbers should I sum up?") 
+x = int(input())
+
+
+
+counter = 1
+y = 0
+while counter < x:
+ print ("Please enter number {} of {}" .format (counter, x))
+ input()
+ counter +=1
+print("The answer is ")
