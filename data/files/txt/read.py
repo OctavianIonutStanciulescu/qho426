512853558bc9437774cbee7c07c7564945436998
OctavianSta <>
2021-08-04 12:28:29 +0000

    Added code to demonstrate how to read a text file.

---
+++ data/files/txt/read.py
@@ -0,0 +1,12 @@
+#reading a text file
+
+def search(locations):
+  print("Searching...\n")
+  with open("data/files/txt/locations.txt") as file:
+    data = file.readlines()
+    for line in data:
+      print("Looked in", line.strip())
+  print ("\n...Done")
+def run():
+  search("data/files/txt/locations.txt")
+run()
