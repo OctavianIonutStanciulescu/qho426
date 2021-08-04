71aa5e9052451d319de0b453cd2ebe8e1f2a86aa
OctavianSta <>
2021-08-04 11:46:34 +0000

    Added code to display information about the current working directory.

---
+++ data/dicts/nested_dicts(pending).py
@@ -0,0 +1,23 @@
+
+# #Creating a simple dictionary
+
+# def pattern():
+#   sequences = {"Short Sequence":3, "Medium Sequence":2, "Long Sequence":1}
+#   return sequences
+
+# def run():
+#   print (pattern())
+
+# run()
+
+
+wedding = {
+    "items":{"Cake":1, "Car":1, "Ribbons":100},
+    "people":{"guests":30}
+}
+for key, value in wedding.items():
+    print(f"{key}: {value}")
+
+
+def short_pattern():
+  parttern = {sequ}
